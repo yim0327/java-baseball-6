eda188b26fb2c40ec05789afe724bf2b10c6259b
yim0327 <yim0327@naver.com>
2025-09-20 20:13:04 +0900

    feat: AnswerProvider 정답 생성 및 NumberRules 상수 정의
    - AnswerProvider
      - setAnswer()에서 pickNumberInRange() 활용해 1~9 범위 숫자 생성
      - 중복 없는 세 자릿수 생성
      - clear()로 게임 재시작 시 리스트 초기화
      - getAnswer()로 불변 리스트 반환
    
    - NumberRules
      - MIN_DIGITS = 1
      - MAX_DIGITS = 9
      - DIGIT_LENGTH = 3

--- src/main/java/baseball/domain/AnswerProvider.java
+++ src/main/java/baseball/domain/AnswerProvider.java
@@ -1,4 +1,27 @@
 package baseball.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class AnswerProvider {
+    private final List<Integer> answer = new ArrayList<>();
+
+    // 정답 생성
+    public void setAnswer() {
+        answer.clear(); // 재시작용 리스트 비우기
+        while (answer.size() < NumberRules.DIGIT_LENGTH) {
+            int randomNumber = Randoms.pickNumberInRange(NumberRules.MIN_DIGITS,
+                    NumberRules.MAX_DIGITS);
+            if (!answer.contains(randomNumber)) {
+                answer.add(randomNumber);
+            }
+        }
+    }
+
+    // 정답 제공
+    public List<Integer> getAnswer() {
+        return List.copyOf(answer);
+    }
 }
--- src/main/java/baseball/domain/NumberRules.java
+++ src/main/java/baseball/domain/NumberRules.java
@@ -1,4 +1,6 @@
 package baseball.domain;
 
 public class NumberRules {
+    public static final int MAX_DIGITS = 9, MIN_DIGITS = 1;
+    public static final int DIGIT_LENGTH = 3;
 }
