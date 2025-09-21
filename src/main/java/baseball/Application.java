154d983011df7332fd547ba2801119359f64f000
yim0327 <yim0327@naver.com>
2025-09-21 16:26:13 +0900

    feat: Player 객체 생성 및 Application 로직 수정
    - Player
      - submittedNum(String)에서 문자열을 List<Integer>로 파싱
      - NumberRules.checkRules()로 검증하고 리스트를 반환
    
    - Application
      - AnswerProvider, Player 객체 생성 및 정답 세팅
      - 콘솔 입력 값을 Player에게 위임

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,26 @@
 package baseball;
 
+import baseball.domain.AnswerProvider;
+import baseball.domain.Player;
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        AnswerProvider computer = new AnswerProvider();
+        Player player = new Player();
+
+        computer.setAnswer();
+
+        System.out.println("숫자 야구 게임을 시작합니다.");
+
+        while (true) {
+            System.out.println("숫자를 입력해주세요 : ");
+            String input = Console.readLine();
+            List<Integer> ball = player.submittedNum(input);
+
+        }
+
     }
 }
--- src/main/java/baseball/domain/Player.java
+++ src/main/java/baseball/domain/Player.java
@@ -1,4 +1,18 @@
 package baseball.domain;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Player {
+    private final NumberRules rules = new NumberRules();
+
+    public List<Integer> submittedNum(String input) {
+        List<Integer> parsedNum = Arrays.stream(input.split(""))
+                .map(Integer::parseInt)
+                .toList();
+
+        rules.checkRules(parsedNum);
+
+        return parsedNum;
+    }
 }
